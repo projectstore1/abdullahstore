7cfa254113576347b1353bc19f5a32b1f1ca14f2
projectstore1 <newgamil41@gmail.com>
2026-08-28 09:23:05 +0600

    Update checkout.js

--- assets/js/checkout.js
+++ assets/js/checkout.js
@@ -1,7 +1,7 @@
 import { db, doc, getDoc, collection, addDoc } from './firebase.js';
 
 // 👇 এখানে আপনার Google Apps Script URL দিন
-const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbzpiBT6c0sK1mmf_AhBkS_XH9-TIrCZVLxDKEgDhmddJg6tbwOH4dEdm_oh1YWZ6D1p/exec'; // যেমন: https://script.google.com/macros/s/XXXX/exec
+const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbwxyAHRk4Qbg7LLO16mEMmTd7CeBBwHTameh1nCzoGlwLf_U0lHbWQLyXKUY9esWg-IXw/exec'; // যেমন: https://script.google.com/macros/s/XXXX/exec
 
 // Cart বা Single Product থেকে ডাটা নেওয়া
 let cart = JSON.parse(localStorage.getItem('cart')) || [];
